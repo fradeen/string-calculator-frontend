cba65c8be5d867061275208c520edbdadb7caf17
Mohd Fardeen <frad.fardeen@gmail.com>
2024-10-20 08:50:27 +0000

    add config/dependencies for unit testing.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+/// reference types=”vitest” />
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts'
+  }
 })
